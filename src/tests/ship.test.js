101843bcbbb59ce0e364c929223ec134922cf5dd
Sofia <117224812+SleepyCasper@users.noreply.github.com>
2026-06-11 18:32:50 +0300

    Add tests for Gameboard and Ship classes to validate functionality

---
+++ src/tests/ship.test.js
@@ -0,0 +1,25 @@
+import { Ship } from "../ship";
+
+test("Is hit", () => {
+    const ship = new Ship(4);
+    ship.hit()
+    expect(ship.hits).toBe(1)
+})
+
+test("Is sunk", () => {
+    const ship = new Ship(4);
+    for(let i = 0; i < 4; i++ ) {
+        ship.hit()
+    }
+
+    expect(ship.isSunk()).toEqual(true)
+})
+
+test("Already sunk error", () => {
+    const ship = new Ship(4);
+    for(let i = 0; i < 4; i++ ) {
+        ship.hit()
+    }
+
+    expect(() => ship.hit()).toThrow("This ship is already sunk")
+})
